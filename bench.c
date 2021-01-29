3f5de7b0f12af8dacbf203050b67fbd91324da8c
Moonchild <elronnd@elronnd.net>
2021-01-28 20:13:34 -0800

    fancy statistics

--- bench.c
+++ bench.c
@@ -1,24 +1,34 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
-#include <assert.h>
+#include <math.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <x86intrin.h>
 
 extern void *fast_memset(void *s, int c, size_t n);
+#define N1 10
+#define N2 2000
 
-#define bencher(name, fn) \
+#define bencher(before, after, avg, stddev, fn) \
 	for (int i = 0; i < 16; i++) fn(buf, 0, l); \
-	uint64_t name ## _before = __rdtsc(); \
-	for (int i = 0; i < 2000; i++) fn(buf, 0, l); \
-	uint64_t name ## _after = __rdtsc()
+	uint64_t before[N1], after[N1]; \
+	uint64_t avg = 0, stddev = 0; \
+	for (int i = 0; i < N1; i++) { \
+		before[i] = __rdtsc(); \
+		for (int _ = 0; _ < N2; _++) fn(buf, 0, l); \
+		after[i] = __rdtsc(); \
+		avg += after[i] - before[i]; \
+	} \
+	avg /= N1; \
+	for (int i = 0; i < N1; i++) stddev += (after[i] - before[i] - avg) * (after[i] - before[i] - avg); \
+	stddev = sqrt(stddev/N1);
 
 void bench(void *buf, size_t l) {
-	bencher(sys, memset);
-	bencher(fm, fast_memset);
+	bencher(sys_before, sys_after, sys_avg, sys_stddev, memset);
+	bencher(fm_before, fm_after, fm_avg, fm_stddev, fast_memset);
 
-	printf("Sys: %llu cycles; fm: %llu cycles\n", sys_after - sys_before, fm_after - fm_before);
+	printf("Sys: %llu +/- %llu cycles; fm: %llu +/- %llu cycles\n", sys_avg, sys_stddev, fm_avg, fm_stddev);
 }
 
 void test(size_t l) {
--- makefile
+++ makefile
@@ -1,12 +1,12 @@
-CFLAGS := -O3 -fno-builtin
+FLAGS := -O3 -fno-builtin -lm
 
 default: dobench
 
 bench: bench.c memset.s
-	$(CC) $(CFLAGS) -o bench bench.c memset.s
+	$(CC) $(FLAGS) -o bench bench.c memset.s
 dobench: bench
 	./bench
 test: test.c memset.s
-	$(CC) $(CFLAGS) -o test test.c memset.s
+	$(CC) $(FLAGS) -o test test.c memset.s
 dotest: tst
 	./test
